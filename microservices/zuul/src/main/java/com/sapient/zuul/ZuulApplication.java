45dc2bec88573feae022f52860b16bf4fcf00d15
Manish Yadav <manyadav5@publicisgroupe.net>
2019-11-12 14:14:27 +0530

    zuul practice

---
+++ microservices/zuul/src/main/java/com/sapient/zuul/ZuulApplication.java
@@ -0,0 +1,41 @@
+package com.sapient.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.sapient.zuul.filters.ErrorFilter;
+import com.sapient.zuul.filters.PostFilter;
+import com.sapient.zuul.filters.PreFilter;
+import com.sapient.zuul.filters.RouteFilter;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApplication.class, args);
+	}
+
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
+
+	@Bean
+	public PostFilter postFilter() {
+		return new PostFilter();
+	}
+
+	@Bean
+	public ErrorFilter errorFilter() {
+		return new ErrorFilter();
+	}
+
+	@Bean
+	public RouteFilter routeFilter() {
+		return new RouteFilter();
+	}
+
+}
