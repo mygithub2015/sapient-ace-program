96c1587b42e2561501ef5c10b9649f0fe9846457
Manish Yadav <manyadav5@publicisgroupe.net>
2019-11-12 16:54:36 +0530

    zuul through eureka try

--- microservices/zuul/src/main/java/com/sapient/zuul/ZuulApplication.java
+++ microservices/zuul/src/main/java/com/sapient/zuul/ZuulApplication.java
@@ -2,6 +2,7 @@ package com.sapient.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
@@ -12,6 +13,7 @@ import com.sapient.zuul.filters.RouteFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableEurekaClient
 public class ZuulApplication {
 
 	public static void main(String[] args) {
