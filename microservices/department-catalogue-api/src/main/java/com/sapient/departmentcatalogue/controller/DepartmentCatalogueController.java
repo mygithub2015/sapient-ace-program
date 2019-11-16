72953423c0f30509b340841f5b86b0a9fc52fd27
mygithub2015 <manishyadav1990@outlook.com>
2019-11-16 17:49:25 +0530

    fixed eureka client calling other by its application name instead of hostname

--- microservices/department-catalogue-api/src/main/java/com/sapient/departmentcatalogue/controller/DepartmentCatalogueController.java
+++ microservices/department-catalogue-api/src/main/java/com/sapient/departmentcatalogue/controller/DepartmentCatalogueController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.discovery.EurekaClient;
 import com.sapient.departmentcatalogue.models.Department;
 import com.sapient.departmentcatalogue.models.DepartmentCatalogue;
 import com.sapient.departmentcatalogue.models.DepartmentCount;
@@ -19,6 +20,8 @@ import com.sapient.departmentcatalogue.models.DepartmentDescription;
 @RestController
 @RequestMapping("department-catalogue-api/")
 public class DepartmentCatalogueController {
+	@Autowired
+    private EurekaClient discoveryClient;
 	
 	private List<DepartmentCatalogue> deptCatalogueList = Arrays.asList(
 															new DepartmentCatalogue(1L, "Java"),
@@ -32,20 +35,26 @@ public class DepartmentCatalogueController {
 	private Department getDeptInformations(@PathVariable Long id) {
 		
 		
-		  DepartmentCount deptCount =
-		  this.restTemplate.getForObject("http://localhost:8081/department-count-api/"+
-		  id, DepartmentCount.class ); DepartmentDescription deptDesc =
-		  this.restTemplate.getForObject(
-		  "http://localhost:8082/department-description-api/"+id,
-		  DepartmentDescription.class);
-		 
 		/*
-		 * DepartmentCount deptCount = this.restTemplate.getForObject(
-		 * "http://department-count-api/department-count-api/"+id, DepartmentCount.class
-		 * ); DepartmentDescription deptDesc = this.restTemplate.getForObject(
-		 * "http://department-description-api/department-description-api/"+id,
+		 * DepartmentCount deptCount =
+		 * this.restTemplate.getForObject("http://localhost:8081/department-count-api/"+
+		 * id, DepartmentCount.class ); DepartmentDescription deptDesc =
+		 * this.restTemplate.getForObject(
+		 * "http://localhost:8082/department-description-api/"+id,
 		 * DepartmentDescription.class);
 		 */
+		 
+        String departmentCountApiUrl = discoveryClient.getNextServerFromEureka("department-count-api", false).getHomePageUrl();
+        String departmentDescApiUrl = discoveryClient.getNextServerFromEureka("department-description-api", false).getHomePageUrl();
+        System.err.println("departmentCountApiUrl: "+departmentCountApiUrl);
+        System.err.println("departmentDescApiUrl: "+departmentDescApiUrl);
+        
+        DepartmentCount deptCount = this.restTemplate.getForObject(
+				  departmentCountApiUrl+"/department-count-api/"+id, DepartmentCount.class
+		  ); DepartmentDescription deptDesc = this.restTemplate.getForObject(
+				  departmentDescApiUrl+"/department-description-api/"+id,
+		  DepartmentDescription.class);
+		 
 		Optional<DepartmentCatalogue> optionalDeptCatalogue = this.deptCatalogueList.stream().filter(e->e.getId().equals(id)).findFirst();
 		DepartmentCatalogue deptCatalogue = new DepartmentCatalogue();
 		if(optionalDeptCatalogue.isPresent())deptCatalogue = optionalDeptCatalogue.get();
